bc055e6bc13a54f5293e5698d62c3dca61b657cd
Neha Ranade <904nehar@gmail.com>
2022-02-16 23:52:31 +0000

    Added option to chose question type in create question

--- client/src/components/EditQuestionComponent.tsx
+++ client/src/components/EditQuestionComponent.tsx
@@ -1,5 +1,6 @@
 import {
     ChoiceGroup,
+    DefaultButton,
     IChoiceGroupOption,
     IconButton,
     ITextFieldStyles,
@@ -10,6 +11,7 @@ import {
 } from "@fluentui/react";
 import {Question} from "../model/Question";
 import {Col, Container, Row} from "react-grid-system";
+import { useState } from "react";
 
 const optionRootClass = mergeStyles({display: 'flex', alignItems: 'baseline'});
 const textFieldStyles: Partial<ITextFieldStyles> = {fieldGroup: {width: 350}};
@@ -22,9 +24,12 @@ interface EditQuestionComponentProps {
     setQuestion: (f: (oldValue: Question) => Question) => void;
 }
 
+
 export const EditQuestionComponent = (
     {question, setQuestion}: EditQuestionComponentProps
 ) => {
+    const [isMCQ, setMCQ] = useState<boolean>(false);
+    const [isLA, setLA] = useState<boolean>(false);
     const createOptions = (): IChoiceGroupOption[] => {
         const createOneOption = (optionId: number): IChoiceGroupOption => {
             return {
@@ -72,6 +77,73 @@ export const EditQuestionComponent = (
         }
         setQuestion(q => ({...q, answer: answer}))
     }
+
+    function McqDisplay(isOn:boolean) {
+        // setLA(false); 
+        if (isOn){
+            return <> <Row>
+            <Col md={2}>
+                <Label style={{ textAlign: "left" }}>Options</Label>
+            </Col>
+            <Col md={6}>
+                <ChoiceGroup
+                    options={createOptions()}
+                    required={true}
+                    selectedKey={`${question.answer}`}
+                    onChange={updateCorrectAnswer} />
+            </Col>
+        </Row><br /><Row>
+                <Col md={2} />
+                <Col md={6}>
+                    <PrimaryButton text="Add option" onClick={() => setQuestion(
+                        q => ({ ...q, options: [...q.options, ''] })
+                    )} />
+                </Col>
+            </Row></>
+        }
+        return <></>
+
+    }
+
+    function LaDisplay(isOn:boolean){ 
+        if (isOn){
+            return <Row>
+                <Col md={2}>
+                    <Label style={{textAlign: "left"}}>Answer</Label>
+                </Col>
+                <Col md={6}>
+                    <TextField
+                        id="question-name-input"
+                        value={"Enter answer here"}
+                        onChange={(_: any, newValue?: string) =>
+                            setQuestion(q => ({...q, name: newValue || ''}))
+                        }
+                    />
+                </Col>
+            </Row>
+
+        }
+        return <></>
+    }
+
+    function handleStates(type:String){
+        if (type === "MCQ" || type === "TF"){
+            if (isLA === true){
+                setLA((isLA) => !isLA);
+            }
+            if (isMCQ === true){
+                setMCQ((isMCQ) => !isMCQ); 
+            }
+            setMCQ((isMCQ) => !isMCQ); 
+        }
+        else if (type === "LA" || type === "SA"){
+            if (isMCQ === true){
+                setMCQ((isMCQ) => !isMCQ); 
+            }
+            setLA((isLA) => !isLA);
+        }
+    }
+
     return (<Container style={{margin: '30px', position: 'relative'}}>
             <Row>
                 <Col md={2}>
@@ -119,31 +191,26 @@ export const EditQuestionComponent = (
                             setQuestion(q => ({...q, textType: newValue || ''}))
                         }
                     />
-
                 </Col>
             </Row>
             <br/>
             <Row>
                 <Col md={2}>
-                    <Label style={{textAlign: "left"}}>Options</Label>
+                    <Label style={{textAlign: "left"}}>Question Type</Label>
                 </Col>
-                <Col md={6}>
-                    <ChoiceGroup
-                        options={createOptions()}
-                        required={true}
-                        selectedKey={`${question.answer}`}
-                        onChange={updateCorrectAnswer}/>
-                </Col>
+                <Col md={2}> 
+                <DefaultButton target="_blank" onClick={() => handleStates("MCQ")} > Multiple Choice</DefaultButton>
+                </Col> 
+                <Col md={2}> 
+                <DefaultButton target="_blank" onClick={() => handleStates("TF")} > True or False</DefaultButton>
+                </Col> 
+                <Col md={2}> 
+                <DefaultButton target="_blank" onClick={() => handleStates("LA")}> Long/Short Answer</DefaultButton>
+                </Col> 
             </Row>
             <br/>
-            <Row>
-                <Col md={2}/>
-                <Col md={6}>
-                    <PrimaryButton text="Add option" onClick={() => setQuestion(
-                        q => ({...q, options: [...q.options, '']})
-                    )}/>
-                </Col>
-            </Row>
+            {McqDisplay(isMCQ)}
+            {LaDisplay(isLA)}
         </Container>
     );
 }
